4a3332364b59be6d939ffd9ab0f2c012407101ff
luxmedia <info@lux-media.de>
2014-12-23 15:16:43 +0100

    major changes in grunt file
    - load-grunt-config implemented
    - default variables in stylus files added

---
+++ grunt/_BAK/BAK_cssmin.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // CSS minifaction
+  files: {
+    '<%= lm_foundation.css_dist_file_min %>': [ '<%= lm_foundation.css_dist_folder %>/*.css' ]
+  }
+}
--- grunt/cssmin.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    // CSS minifaction
-    cssmin: {
-        files: {
-            '<%= lm_foundation.css_dist_file_min %>': [ '<%= lm_foundation.css_dist_folder %>/*.css' ]
-        }
-    }
-}
