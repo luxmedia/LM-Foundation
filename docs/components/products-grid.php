69a2dd833bde33ce805f72e7f64ba3ce484fb46f
luxmedia <info@lux-media.de>
2015-05-20 20:10:01 +0200

    Productlist styling refined

--- docs/components/products-grid.php
+++ docs/components/products-grid.php
@@ -2,10 +2,12 @@
 <li class="col sml-12 med-6 lrg-4 xlrg-3" itemscope itemtype="http://schema.org/Product">
 	<div class="prd">
 	<a class="prd__link">
-		<span class="prd__img"><img itemprop="image" src="" width="auto" height="100" /></span>
-		<span class="prd__brand" itemprop="brand">brand</span>
+		<div class="prd__img">
+			<img itemprop="image" src="http://placehold.it/220x120" />
+			<div class="fig-overlay"><button class="btn btn--outline btn--round btn__details">Details</button></div>
+		</div>
+		<div class="prd__brand" itemprop="brand">brand</div>
 		<h3 itemprop="name" class="prd__title">Product with special price</h3>
-		<button class="btn btn__details">Details</button>
 		<div class="price-box">
 			<span itemprop="price" class="price price--special">99,- &euro;</span>
 			<span class="price price--old"><span class="price__label price__label--uvp">UVP:&nbsp;</span>118,80 &euro;</span>
@@ -18,10 +20,12 @@
 <li class="col sml-12 med-6 lrg-4 xlrg-3" itemscope itemtype="http://schema.org/Product">
 	<div class="prd">
 	<a class="prd__link">
-		<span class="prd__img"><img itemprop="image" src="" width="auto" height="100" /></span>
-		<span class="prd__brand" itemprop="brand">brand</span>
+		<div class="prd__img">
+			<img itemprop="image" src="http://placehold.it/220x120" />
+			<div class="fig-overlay"><button class="btn btn--outline btn--round btn__details">Details</button></div>
+		</div>
+		<div class="prd__brand" itemprop="brand">brand</div>
 		<h3 itemprop="name" class="prd__title">Longer product title which can be max 2 lines</h3>
-		<button class="btn btn__details">Details</button>
 		<div class="price-box">
 			<span itemprop="price" class="price price--special">299,- &euro;</span>
 		</div>
@@ -32,10 +36,12 @@
 <li class="col sml-12 med-6 lrg-4 xlrg-3" itemscope itemtype="http://schema.org/Product">
 	<div class="prd">
 	<a class="prd__link">
-		<span class="prd__img"><img itemprop="image" src="" width="auto" height="100" /></span>
-		<span class="prd__brand" itemprop="brand">brand</span>
+		<div class="prd__img">
+			<img itemprop="image" src="http://placehold.it/220x120" />
+			<div class="fig-overlay"><button class="btn btn--outline btn--round btn__details">Details</button></div>
+		</div>
+		<div class="prd__brand" itemprop="brand">brand</div>
 		<h3 itemprop="name" class="prd__title">Product with minimal price</h3>
-		<button class="btn btn__details">Details</button>
 		<div class="price-box">
 			<span class="price price--minimal"><span itemprop="lowPrice" class="price__label price__label--minimal">ab&nbsp;</span>159,- &euro;</span>
 		</div>
@@ -46,10 +52,12 @@
 <li class="col sml-12 med-6 lrg-4 xlrg-3" itemscope itemtype="http://schema.org/Product">
 	<div class="prd prd--special prd--new">
 	<a class="prd__link">
-		<span class="prd__img"><img itemprop="image" src="" width="auto" height="100" /></span>
-		<span class="prd__brand" itemprop="brand">brand</span>
+		<div class="prd__img">
+			<img itemprop="image" src="http://placehold.it/220x120" />
+			<div class="fig-overlay"><button class="btn btn--outline btn--round btn__details">Details</button></div>
+		</div>
+		<div class="prd__brand" itemprop="brand">brand</div>
 		<h3 itemprop="name" class="prd__title">New Product with special price</h3>
-		<button class="btn btn__details">Details</button>
 		<div class="price-box">
 			<span class="price price--special"><span class="price__label price__label--special">nur&nbsp;</span><span itemprop="price">159,- &euro;</span></span>
 			<span class="price price--old"><span class="price__label price__label--uvp">UVP&nbsp;</span>159,- &euro;</span>
--- docs/components/products-list.php
+++ docs/components/products-list.php
@@ -2,8 +2,11 @@
 	<li class="prd cf" itemscope itemtype="http://schema.org/Product">
 		<a class="prd__link">
 			<div class="prd__info">
-				<span class="prd__img"><img itemprop="image" src="" width="auto" height="100" /></span>
-				<span class="prd__brand" itemprop="brand">brand</span>
+				<div class="prd__img">
+					<img itemprop="image" src="http://placehold.it/140x140" />
+					<div class="fig-overlay"><button class="btn btn--outline btn--round btn__details">Details</button></div>
+				</div>
+				<div class="prd__brand" itemprop="brand">brand</div>
 				<h3 itemprop="name" class="prd__title">Product with special price</h3>
 				<div class="prd__sdesc" itemprop="description"><p>This is a short description about the products key feautres and benefits without beeing too long. Just a brief overview.</p></div>
 				<div class="prd__opts"><span class="lbl">Varianten:&nbsp;</span>
@@ -20,7 +23,7 @@
 				<span itemprop="price" class="price price--special">99,- &euro;</span>
 				<span class="price price--old"><span class="price__label price__label--uvp">UVP:&nbsp;</span>118,80 &euro;</span>
 			</div>
-			<button class="btn btn__details">Details</button>
+			
 			<div class="ribbon ribbon--primary">-20%</div>
 		</a>
 	</li>
@@ -28,38 +31,47 @@
 	<li class="prd cf" itemscope itemtype="http://schema.org/Product">
 		<a class="prd__link">
 			<div class="prd__info">
-				<span class="prd__img"><img itemprop="image" src="" width="auto" height="100" /></span>
-				<span class="prd__brand" itemprop="brand">brand</span>
+				<div class="prd__img">
+					<img itemprop="image" src="http://placehold.it/140x140" />
+					<div class="fig-overlay"><button class="btn btn--outline btn--round btn__details">Details</button></div>
+				</div>
+				<div class="prd__brand" itemprop="brand">brand</div>
 				<h3 itemprop="name" class="prd__title">Longer product title which can be max 2 lines</h3>
 				<div class="prd__sdesc" itemprop="description"><p>This is a short description about the products key feautres and benefits without beeing too long. Just a brief overview.</p></div>
 			</div>
 			<div class="price-box">
 				<span itemprop="price" class="price price--special">299,- &euro;</span>
 			</div>
-			<button class="btn btn__details">Details</button>
+			
 		</a>
 	</li>
 
 	<li class="prd cf" itemscope itemtype="http://schema.org/Product">
 		<a class="prd__link">
 			<div class="prd__info">
-				<span class="prd__img"><img itemprop="image" src="" width="auto" height="100" /></span>
-				<span class="prd__brand" itemprop="brand">brand</span>
+				<div class="prd__img">
+					<img itemprop="image" src="http://placehold.it/140x140" />
+					<div class="fig-overlay"><button class="btn btn--outline btn--round btn__details">Details</button></div>
+				</div>
+				<div class="prd__brand" itemprop="brand">brand</div>
 				<h3 itemprop="name" class="prd__title">Product with minimal price</h3>
 				<div class="prd__sdesc" itemprop="description"><p>This is a short description about the products key feautres and benefits without beeing too long. Just a brief overview.</p></div>
 			</div>
 			<div class="price-box">
 				<span class="price price--minimal"><span itemprop="lowPrice" class="price__label price__label--minimal">ab&nbsp;</span>159,- &euro;</span>
 			</div>
-			<button class="btn btn__details">Details</button>
+			
 		</a>
 	</li>
 
 	<li class="prd prd--special prd--new cf" itemscope itemtype="http://schema.org/Product">
 		<a class="prd__link">
 			<div class="prd__info">
-				<span class="prd__img"><img itemprop="image" src="" width="auto" height="100" /></span>
-				<span class="prd__brand" itemprop="brand">brand</span>
+				<div class="prd__img">
+					<img itemprop="image" src="http://placehold.it/140x140" />
+					<div class="fig-overlay"><button class="btn btn--outline btn--round btn__details">Details</button></div>
+				</div>
+				<div class="prd__brand" itemprop="brand">brand</div>
 				<h3 itemprop="name" class="prd__title">New Product with special price</h3>
 				<div class="prd__sdesc" itemprop="description"><p>This is a short description about the products key feautres and benefits without beeing too long. Just a brief overview.</p></div>
 			</div>
@@ -67,7 +79,7 @@
 				<span class="price price--special"><span class="price__label price__label--special">nur&nbsp;</span><span itemprop="price">159,- &euro;</span></span>
 				<span class="price price--old"><span class="price__label price__label--uvp">UVP&nbsp;</span>159,- &euro;</span>
 			</div>
-			<button class="btn btn__details">Details</button>
+			
 			<div class="ribbon ribbon--warning">Neu!</div>
 			<div class="ribbon ribbon--primary">-20%</div>
 		</a>
